3473a70cadf6987eb84a06254cfe85df1a13c334
notweuz <weuz.main@gmail.com>
2025-05-06 02:43:23 +0400

    Add Item Controller

--- src/main/java/ru/practicum/shareit/item/ItemController.java
+++ src/main/java/ru/practicum/shareit/item/ItemController.java
@@ -1,7 +1,15 @@
 package ru.practicum.shareit.item;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.item.dto.ItemCreateDto;
+import ru.practicum.shareit.item.dto.ItemDto;
+import ru.practicum.shareit.item.dto.ItemUpdateDto;
+import ru.practicum.shareit.item.model.Item;
+
+import java.util.List;
 
 /**
  * TODO Sprint add-controllers.
@@ -9,4 +17,39 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/items")
 public class ItemController {
+    private final ItemService itemService;
+
+    @Autowired
+    public ItemController(ItemService itemService) {
+        this.itemService = itemService;
+    }
+
+    @PostMapping
+    public ItemDto create(@Valid @RequestBody ItemCreateDto item, @RequestHeader("X-Sharer-User-Id") Long userId) {
+        return ItemDto.toItemDto(itemService.create(ItemCreateDto.toItem(item), userId));
+    }
+
+    @GetMapping("/{itemId}")
+    public ItemDto findById(@PathVariable Long itemId) {
+        return ItemDto.toItemDto(itemService.findById(itemId));
+    }
+
+    @GetMapping
+    public List<ItemDto> findAllByUser(@RequestHeader("X-Sharer-User-Id") Long userId) {
+        return itemService.findAllByUser(userId).stream()
+                .map(ItemDto::toItemDto)
+                .toList();
+    }
+
+    @PatchMapping("/{itemId}")
+    public ItemDto update(@PathVariable Long itemId, @RequestBody ItemUpdateDto item, @RequestHeader("X-Sharer-User-Id") Long userId) {
+        return ItemDto.toItemDto(itemService.update(ItemUpdateDto.toItem(item), itemId, userId));
+    }
+
+    @GetMapping("/search")
+    public List<ItemDto> search(@RequestParam String text) {
+        return itemService.search(text).stream()
+                .map(ItemDto::toItemDto)
+                .toList();
+    }
 }
